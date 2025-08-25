c5292499b2dcca7dba09e4aedb265a52d12a4643
taleks1 <135526584+taleks1@users.noreply.github.com>
2025-08-25 17:21:36 +0200

    index vo layou

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { LogOut, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
