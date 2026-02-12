32aca0c0fcd6af2757787e7e168494ee115f567e
David Anastasov <davidanastasov@hotmail.com>
2026-02-12 16:41:02 +0100

    Redirects root route to dashboard on load

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: App,
+  beforeLoad: () => {
+    throw redirect({ to: "/dashboard" });
+  },
 });
-
-function App() {
-  return <div>Hello "/"!</div>;
-}
